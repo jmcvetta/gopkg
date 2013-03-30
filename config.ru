ed10c59fcfcaa072f2efadcb908af82b52091006
Jason McVetta <jason.mcvetta@gmail.com>
2013-03-29 17:05:24 -0700

    license

--- config.ru
+++ config.ru
@@ -1,3 +1,7 @@
+# Copyright (c) 2013 Jason McVetta.  This is Free Software, released under the
+# terms of the AGPL v3.  See www.gnu.org/licenses/agpl-3.0.html for details.
+# Resist intellectual serfdom - the ownership of ideas is akin to slavery.
+
 use Rack::ShowExceptions
 
 require './gopkg'
--- heroku.sh
+++ heroku.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+#
+# Copyright (c) 2013 Jason McVetta.  This is Free Software, released under the
+# terms of the AGPL v3.  See www.gnu.org/licenses/agpl-3.0.html for details.
+# Resist intellectual serfdom - the ownership of ideas is akin to slavery.
+#
+
 heroku create --buildpack https://github.com/ddollar/heroku-buildpack-multi.git $1
